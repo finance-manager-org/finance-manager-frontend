17511b6c7896139140bbcb1efef8ce21b0274d4e
DanielTrujillo01 <jose.trujillo@correounivalle.edu.co>
2025-11-05 23:32:02 -0500

    Landing page V1

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Hero } from "./components/Hero";
+import { Features } from "./components/Features";
+import { HowItWorks } from "./components/HowItWorks";
+import { Benefits } from "./components/Benefits";
+import { CTA } from "./components/CTA";
+import { Footer } from "./components/Footer";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-slate-50 to-white">
+      <Hero />
+      <Features />
+      <Benefits />
+      <HowItWorks />
+      <CTA />
+      <Footer />
+    </div>
+  );
+}
